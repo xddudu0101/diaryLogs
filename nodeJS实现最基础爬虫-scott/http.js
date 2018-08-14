cf834e4b11f4125517144b33e769d201db2ae14d
DZ <2365113706@qq.com>
2018-08-15 07:28:01 +0800

    node基础版爬虫

---
+++ nodeJS实现最基础爬虫-scott/http.js
@@ -0,0 +1,25 @@
+var http = require('http')
+
+var url = 'http://www.runoob.com/nodejs/nodejs-tutorial.html';
+
+http.get(url,function(res){
+
+    var html=''
+
+    res.on('data',function(data){
+
+        html += data
+
+    })
+
+    res.on('end',function(){
+
+        console.log(html)
+
+    })
+
+}).on('error',function(){
+
+    console.log('获取资源出错！')
+
+})
